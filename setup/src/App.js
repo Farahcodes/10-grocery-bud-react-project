43073a14ddc0b84cfa4a501e118237bd8aebc5d7
farah <farah.fournat@gmail.com>
2022-01-13 12:11:07 +0100

    conditional rendering of the list

--- setup/src/App.js
+++ setup/src/App.js
@@ -35,12 +35,14 @@ function App() {
           </button>
         </div>
       </form>
+      {list.length > 0 &&
       <div className="grocery-container">
         <List items ={list}/>
         <button className="clear-btn">
           clear items
         </button>
       </div>
+      }
     </section>
   )
 }
