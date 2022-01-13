735ac2fad8a3be6846a36c0c6a0d913dee53add3
farah <farah.fournat@gmail.com>
2022-01-13 20:37:24 +0100

    added clear list function

--- setup/src/App.js
+++ setup/src/App.js
@@ -18,7 +18,7 @@ function App() {
       //deal with edit
     }
     else{
-      //show alert
+      showAlert(true,'success','item added to the list')
       const newItem ={id : new Date().getTime().toString(),title:name};
       setList([...list,newItem])
       setName('')
@@ -27,6 +27,10 @@ function App() {
 
   const showAlert=(show=false,type='',msg='')=>{
     setAlert({show,type,msg})
+  }
+  const clearList = ()=>{
+    showAlert(true,'danger','empty list');
+    setList([])
   }
   return (
     <section className="section-center">
@@ -43,7 +47,7 @@ function App() {
       {list.length > 0 &&
       <div className="grocery-container">
         <List items ={list}/>
-        <button className="clear-btn">
+        <button className="clear-btn" onClick={clearList}>
           clear items
         </button>
       </div>
