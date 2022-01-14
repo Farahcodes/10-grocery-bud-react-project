f4c243e6e922983ca1a500a56dea1c309aead4c3
farah <farah.fournat@gmail.com>
2022-01-14 16:45:40 +0100

    added edit button functionality

--- setup/src/App.js
+++ setup/src/App.js
@@ -16,6 +16,18 @@ function App() {
     }
     else if(name && isEditing){
       //deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
     }
     else{
       showAlert(true,'success','item added to the list')
@@ -36,6 +48,12 @@ function App() {
     showAlert(true,'danger','item removed')
     setList(list.filter((item)=> item.id !== id))
   }
+  const editItem = (id)=>{
+    const specificItem = list.find((item)=> item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title)
+  }
   return (
     <section className="section-center">
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -50,7 +68,7 @@ function App() {
       </form>
       {list.length > 0 &&
       <div className="grocery-container">
-        <List items ={list} removeItem={removeItem}/>
+        <List items ={list} removeItem={removeItem} editItem={editItem} />
         <button className="clear-btn" onClick={clearList}>
           clear items
         </button>
